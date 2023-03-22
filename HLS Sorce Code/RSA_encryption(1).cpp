504e79191ea155fc5ef23c4d5774929cc02ab059
noahe7700 <126737528+noahe7700@users.noreply.github.com>
2023-03-22 16:38:53 -0400

    Update RSA_encryption(1).cpp

--- HLS Sorce Code/RSA_encryption(1).cpp
+++ HLS Sorce Code/RSA_encryption(1).cpp
@@ -4,8 +4,8 @@
 This is a similiar implementation of the primary code with the
 exception that the keys are provided rather than generated.
 The cause for this change was challenges generating the bit
-stream in vivado. When exporting RTL and synthesizing, there 
-were many issues with pipelining and scheduling. My hope is 
+stream in vivado. When exporting RTL and synthesizing, there
+were many issues with pipelining and scheduling. My hope is
 that by limiting the number of recursive logic performed, the
 code will synthesize properly and generate an overlay.
 ##############################################################
@@ -26,14 +26,16 @@ using namespace std;
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
-typedef ap_axis<32,1,1,1> AXI_VAL;
-typedef int data_t;
-typedef int	coef_t;
-typedef int	acc_t;
+typedef ap_axis<64,1,1,1> AXI_VAL;
+//typedef int data_t;
+//typedef int	coef_t;
+//typedef int	acc_t;
+typedef ap_uint<65> uint65_t;
+typedef ap_uint<128> uint128_t;
 typedef ap_uint<512> uint512_t;
 typedef ap_uint<1024> uint1024_t;
 
-typedef ap_fixed<64, 32, AP_RND_CONV, AP_SAT> fixed_t;
+//typedef ap_fixed<64, 32, AP_RND_CONV, AP_SAT> fixed_t;
 /*
 uint1024_t inv_mod(uint1024_t a, uint1024_t m){
 	uint1024_t m0 = m;
@@ -74,6 +76,7 @@ uint1024_t inv_mod(uint1024_t a, uint1024_t m){
 
 }
 */
+/*
 uint1024_t gcd(uint1024_t a, uint1024_t b){
 	while(b!=0){
 		uint1024_t t =b;
@@ -82,9 +85,10 @@ uint1024_t gcd(uint1024_t a, uint1024_t b){
 	}
 	return a;
 }
+*/
 
-uint1024_t mod_exp(uint1024_t a, uint1024_t b, uint1024_t m){
-	uint1024_t res = 1;
+uint128_t mod_exp(uint128_t a, uint128_t b, uint128_t m){
+	uint128_t res = 1;
 
 	a = a % m;
 
@@ -97,6 +101,7 @@ uint1024_t mod_exp(uint1024_t a, uint1024_t b, uint1024_t m){
 	}
 	return res;
 }
+
 // No longer needed for this version (returned N = 6103297, e = 65537)
 /*
 void generate_key(uint1024_t& n, uint1024_t& e, uint1024_t& d){
@@ -120,23 +125,27 @@ void generate_key(uint1024_t& n, uint1024_t& e, uint1024_t& d){
 
 }
 */
+
 void rsa_encrypt(uint1024_t n, uint1024_t e, AXI_VAL plain, AXI_VAL& cipher){
 	uint1024_t plainVal = plain.data.to_int();
 	cipher.data = mod_exp(plainVal, e, n);
 }
 
-void rsa_decrypt(uint1024_t n, uint1024_t d, uint1024_t cipher, uint512_t& plain){
-	plain = mod_exp(cipher, d, n);
+void rsa_decrypt(uint1024_t n, uint1024_t d, AXI_VAL cipher, AXI_VAL& plain){
+	uint1024_t cipherVal = cipher.data.to_int();
+	plain.data = mod_exp(cipherVal, d, n);
 }
+
 void encrypt (hls::stream<AXI_VAL>& dataIn, hls::stream<AXI_VAL>& dataOut) {
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS INTERFACE axis register both port=dataIn
 #pragma HLS INTERFACE axis register both port=dataOut
-#pragma hls interface s_axilite port=return
-	uint1024_t n, e, d; // Variables used for the keys (since static, we can test with one or the other).
+
+
+	uint128_t n, e, d; // Variables used for the keys (since static, we can test with one or the other).
 	AXI_VAL plain;
 	AXI_VAL cipher;
-	uint512_t decrypted_plain;
+	//uint128_t decrypted_plain;
 	// Removed generate keys... set the keys to the right values...
 	n = 6103297;
 	e = 65537;
@@ -144,8 +153,23 @@ void encrypt (hls::stream<AXI_VAL>& dataIn, hls::stream<AXI_VAL>& dataOut) {
 	while(1){
 		dataIn.read(plain); // Sets the plain to the value read
 
-		rsa_encrypt(n, e, plain, cipher); // Performs the RSA encryption and output encrypted value to cipher
+		n = 6103297;
+		e = 65537;
 
+		//cipher.data = mod_exp(plain.data.to_int(), e, n);
+		uint128_t res = 1;
+		int a, b, m;
+		a = plain.data.to_int();
+
+		a = a%n;
+		while (e > 0){
+				if(e & 1)
+					res = (res * a) % n;
+				e = e >> 1;
+				a = (a * a) % n;
+
+			}
+		cipher.data = res;
 		dataOut.write(cipher); //After performing the encryption, the output stores the cipher
 
 		if(plain.last){
